ba4ffd28d9b530f3fbc6a66c8dfaac0c535c3909
unknown <pavannttf29@gmail.com>
2021-01-30 18:40:18 +0530

    --- Auto-Git Commit ---

---
+++ main.js
@@ -0,0 +1,11 @@
+// hosting
+
+console.log (a); //haveen before execution of  single line of code js engine alloacted memeory to each variable and function
+var a = 10;
+var name = 'pavan ';
+function getName () {
+  // deaclaretion of Function
+  name = 'pavan';
+  console.log ('welocm to' + ' ' + name);
+}
+getName ();
